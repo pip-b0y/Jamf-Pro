946157ba2180b7ab340115df25278d4d3838e351
Hayden Charter <69624918+pip-b0y@users.noreply.github.com>
2022-06-28 15:27:52 +1000

    Update Device-Ran-InTune-or-didnot.sh
    Removed Python Reference. Bash Only

--- MacOS_EAs/Device-Ran-InTune-or-didnot.sh
+++ MacOS_EAs/Device-Ran-InTune-or-didnot.sh
@@ -3,10 +3,13 @@
 #Jamf EA to find if a device has run the company portal Join
 #Please note, This may not be 100% true for measuring compliance. please rely on the device record in Jamf and the device record in Azure/Intune
 #Please not file paths are subject to change because CompanyPortal Application is not owned by Jamf 
-currentuser=$(/usr/bin/python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')
+#Removed Python
+#Added method for getting home folders 
+currentuser=$(stat -f "%Su" /dev/console)
+user_path=$(echo ~${currentuser})
 #Lets check for the file
 
-if [ -f "/Users/$currentuser/Library/Application Support/com.microsoft.CompanyPortalMac.usercontext.info" ]; then
+if [ -f "${user_path}/Library/Application Support/com.microsoft.CompanyPortalMac.usercontext.info" ]; then
 	echo "<result>Machine is Intuned</result>"
 	else
 		echo "<result>Machine is not InTuned</result>"
